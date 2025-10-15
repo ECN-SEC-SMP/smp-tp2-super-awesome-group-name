b27bd23f746bd7d6f357280a5e218c271150c1bf
Jemak56 <jkervil@gmail.com>
2025-10-15 15:08:11 +0200

    Update utilitaires.cpp

--- utilitaires.cpp
+++ utilitaires.cpp
@@ -1,1 +1,20 @@
-r
+#include <iostream>
+#include "/Users/coucoujm/Downloads/tp2-code-20251015/entierlong.h"
+
+ t_EntierLong inttolong (int chiffre){
+    struct t_EntierLong Long; // definition d'une structure Long
+    int div
+    if (chiffre<0){             //controle du signe de notre chiffre d'entré
+        Long.negatif=true; // mise à 1 de la variable negatif, qui corre'spond au signe de notre entier long
+    }
+    while (div != 0 )
+    Long.chiffres[0]=chiffre; // mise de la valeur de chiffre dans notre structure Long
+    return (Long); // on retourne notre structure   
+}
+
+ bool Egalite (t_EntierLong a, t_EntierLong b){
+    bool egal
+    if (a.chiffres == b.chiffres){
+        if 
+    }
+ }
